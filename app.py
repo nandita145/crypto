91ea379aaa112606c6dcab2fd5ed8656d32253fc
nandita-exe <85404089+nandita-exe@users.noreply.github.com>
2022-02-26 10:37:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,12 @@ st.markdown("""
       </div>
     </nav>
 """, unsafe_allow_html=True)
-
+st.markdown("""
+<script
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+crossorigin="anonymous"></script>
+""", unsafe_allow_html=True)
 today = date.today()
 
 d1 = today.strftime("%Y-%m-%d")
